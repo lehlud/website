8f229db36517236d6848cbd3a222069af5c56ee7
lehlud <trash@ludwig-lehnert.de>
2023-03-08 12:01:58 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body>
+  <header id="header">
+    <img id="logo" src="/img/logo.svg" alt="">
+
+    <nav id="nav">
+      <div id="nav-links-wrapper">
+        <ul id="nav-links">
+          <li><a href="https://nx.lehlud.de/">Nextcloud</a></li>
+          <!--<li><a href="https://links.lehlud.de/tagesschau">Tagesschau</a></li>-->
+          <!--<li><a href="https://keys.openpgp.org/search?q=info%40ludwig-lehnert.de">OpenPGP-Schlüssel</a></li>-->
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <?php include __DIR__ . '/animation.php' ?>
+  <main id="content">
+    <div>
+      <h1>Hallihallo</h1>
+      <p>Ich bin der Ludwig.</p>
+    </div>
+  </main>
+
+  <footer id="footer">
+    <ul id="footer-links">
+      <li><a href="/datenschutz/">Datenschutz</a></li>
+      <li><a href="/impressum/">Impressum</a></li>
+    </ul>
+  </footer>
+</body>
+
+</html>
